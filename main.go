7522cbd7d2d00e8510090ae84489aba6979c0ac9
DucSang404 <ndsang404@gmail.com>
2025-04-15 20:11:27 +0700

    new

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
 
 func IsPrime(n int) bool {
 	for i := 2; i*i <= n; i++ {
